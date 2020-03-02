1103f5fe6f057d52c3b4798e8a54bea9a617bdd1
novendrasatria <novendradotid@gmail.com.com>
2020-03-02 16:46:09 +0700

    Pertemuan 5

---
+++ BiodataKu/app/src/main/java/novendra/satria/biodataku/MainActivity.kt
@@ -0,0 +1,12 @@
+package novendra.satria.biodataku
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.biodata)
+    }
+}
