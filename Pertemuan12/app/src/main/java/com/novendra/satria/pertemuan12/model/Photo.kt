acf6771cd5db3ba1c6ec4ca4132770e8aa29ee7d
novendrasatria <novendradotid@gmail.com>
2020-05-04 15:39:01 +0700

    Praktikum Pertemuan 12

---
+++ Pertemuan12/app/src/main/java/com/novendra/satria/pertemuan12/model/Photo.kt
@@ -0,0 +1,13 @@
+package com.novendra.satria.pertemuan12.model
+
+import com.google.gson.annotations.SerializedName
+
+data class Photo (
+    @SerializedName("id")
+    val id: Int?,
+    @SerializedName("title")
+    val title: String?,
+    @SerializedName("thumbnailUrl")
+    val  thumbnail: String?
+
+)
